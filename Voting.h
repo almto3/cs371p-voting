4427736c241e39342edd34f163a1c42b2e2e0e2f
almto3 <almto3@hotmail.com>
2016-02-11 21:11:57 -0600

    addes voting.h

---
+++ Voting.h
@@ -0,0 +1,69 @@
+#ifndef Voting_h
+#define Voting_h
+
+const int MAX_CANDIDATES = 20;
+const int MAX_BALLOTS = 1000;
+
+
+// --------
+// includes
+// --------
+
+#include <iostream> // istream, ostream
+#include <string>   // string
+
+using namespace std;
+
+// ------------
+// struct Case
+// ------------
+/**
+ * add description
+ */
+
+struct Case {
+	int n; 												//number of candidates
+	string [MAX_CANDIDATES] names; 						//names of candidates
+	char [MAX_BALLOTS][MAX_CANDIDATES] ballots; 		//the ballots
+};
+
+// ------------
+// voting_read
+// ------------
+/**
+ * read a std input
+ * @param r an istream
+ * @returns a pointer array of struct Case
+ */
+
+Case* voting_read (istream& r);		
+
+// ------------
+// voting_eval
+// ------------
+/**
+ * @c is is the instance of the struct Case
+ * @returns the winner id in the array of candidates
+ */
+int voting_eval (Case& c);
+
+// -------------
+// voting_print
+// -------------
+/**
+ * @c is is the instance of the struct Case
+ * whatevez
+ */
+void voting_print (Case& c);
+
+// -------------
+// voting_solve
+// -------------
+
+/**
+ * @param r an istream
+ * @param w an ostream
+ */
+void voting_solve (istream& r, ostream& w);
+
+#endif // Voting_h
